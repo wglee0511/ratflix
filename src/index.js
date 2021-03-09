cd735132e611a089b44dc7eca141da8e38be48a7
wglee0511 <ranazang@naver.com>
2021-03-09 22:32:20 +0900

    exaple.html  -> navbar

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+//https://yts-proxy.now.sh/list_movies.json
 ReactDOM.render(
   <React.StrictMode>
     <App />
