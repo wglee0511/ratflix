b8e30f4d077c177b9dbbc9b72542018d7150d188
wglee0511 <ranazang@naver.com>
2021-04-15 23:05:45 +0900

    HashRouter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Navbar from "./components/Navbar";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import Movie from "./components/Movie";
 import TvProgram from "./components/TvProgram";
 import Search from "./components/Search";
